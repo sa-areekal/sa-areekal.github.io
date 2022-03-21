6e9501bfafdab41e936ab20664fd84ee2fd8930b
sa-areekal <a.sandraaravind@students.iiserpune.ac.in>
2022-03-21 10:28:15 +0530

    Public folder updated.

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:/media/sandra/D Drive/Workspace/Website/acad/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5/assets/js/wowchemy-utils.js
+  // ns-hugo:/media/sandra/D Drive/Workspace/Website/sa-areekal.github.io/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
